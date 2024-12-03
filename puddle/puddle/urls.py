6300d6b021a53f727883cbd80f69ead2db6473a7
Tushar V Shelke <109097998+tusharlikestocode@users.noreply.github.com>
2024-12-03 21:51:56 +0530

    uploaded files

---
+++ puddle/puddle/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+urlpatterns = [
+    path('', include('core.urls')),
+    path('dashboard/',include('dashboard.urls')),
+    path('items/',include('item.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
